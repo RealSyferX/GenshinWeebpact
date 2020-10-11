89a4df93071c7c100328ce93d81a5abed7e17dfd
Natural <superlevel2021@gmail.com>
2020-10-12 00:01:27 +0800

    Update DllMain.cpp

--- GenshinInt/DllMain.cpp
+++ GenshinInt/DllMain.cpp
@@ -6,7 +6,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 	//DisableThreadLibraryCalls(hModule);
 	if (dwReason == DLL_PROCESS_ATTACH) {
 		//MessageBoxA(0, "Insider", "haha", 0);
-		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Ready, 0, 0, 0);
+		CreateThread(0, 0, Ready, 0, 0, 0);// Save thread
 	}
 	return TRUE;
 }
